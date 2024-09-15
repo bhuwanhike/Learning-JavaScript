8431ad7cbb52e6f1076d3ead37636ea197ee9218
Bhuwan Sharma <69743169+bhuwanhike@users.noreply.github.com>
2024-09-15 12:19:57 +0530

    for, while, do-while loops

---
+++ #2.SourceCode.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+// taking user input
+let age = prompt("hey there ");
+age = Number.parseInt(age)
+
+// using for loop
+for(let i = 0; i<age; i++){
+  console.log(i)
+}
+
+// using while loop
+let i = 0
+while(i<age){
+  console.log(i)
+  i++
+}
+
+// using do-while loop
+let i = 0
+do{
+  console.log(i)
+  i++
+}while(i<age)
+
